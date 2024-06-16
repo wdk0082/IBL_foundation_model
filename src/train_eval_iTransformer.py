7a4536b40e6b06ec97f1c0ee3a32f775aec407e6
Zixuan Wang <zwang34@exp-1-06.expanse.sdsc.edu>
2024-06-16 08:04:23 -0700

    update

--- src/train_eval_iTransformer.py
+++ src/train_eval_iTransformer.py
@@ -16,7 +16,7 @@ from trainer.make import make_trainer
 from utils.eval_utils import load_model_data_local, co_smoothing_eval, behavior_decoding
 import warnings
 warnings.simplefilter("ignore")
-
+64
 # Fix Args
 EID_PATH = 'data/target_eids.txt'
 
